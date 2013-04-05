3a06bbddc54eed7de80abe1be1687ceb0f7e9f65
Iankulov Miodrag <mutu23@gmail.com>
2013-04-05 21:43:10 +0200

    Finished hash script :-)) hard job

--- hash.php
+++ hash.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * Basic stuff, for a basic job :-)
+ */
 if( isset($_GET['hash']) && !empty($_GET['hash'])){
   $data = array(
-  ''
+  'md5'    => md5($_GET['hash']),
+  'sha1'   => sha1($_GET['hash']),
+  'sha256' => hash('sha256',$_GET['hash']),
+  'sha384' => hash('sha384',$_GET['hash']),
+  'sha512' => hash('sha512',$_GET['hash'])
   );
+}else{
+  $data = array('error' => 'No hash!');
 }
+//B00M!!
+echo json_encode($data);
