6b90f87dab6bc95f7797798debb66762607704f6
Iankulov Miodrag <mio@imiodrag.com>
2014-08-13 22:30:22 +0300

    Added support for passing a hash via url

--- hash.php
+++ hash.php
@@ -6,11 +6,11 @@
 if( isset($_GET['hash']) && !empty($_GET['hash'])){
   $hash = urldecode($_GET['hash']);
   $data = array(
-  'md5'    => md5($hash).' ('.strlen(md5($hash)).')',
-  'sha1'   => sha1($hash) .' ('.strlen(sha1($hash)).')',
-  'sha256' => hash('sha256',$hash).' ('.strlen(hash('sha256',$hash)).')',
-  'sha384' => hash('sha384',$hash).' ('.strlen(hash('sha384',$hash)).')',
-  'sha512' => hash('sha512',$hash).' ('.strlen(hash('sha512',$hash)).')'
+  'md5'    => md5($hash).' ('.strlen(md5($hash)).' string length)',
+  'sha1'   => sha1($hash) .' ('.strlen(sha1($hash)).' string length)',
+  'sha256' => hash('sha256',$hash).' ('.strlen(hash('sha256',$hash)).' string length)',
+  'sha384' => hash('sha384',$hash).' ('.strlen(hash('sha384',$hash)).' string length)',
+  'sha512' => hash('sha512',$hash).' ('.strlen(hash('sha512',$hash)).' string length)'
   );
 }else{
   $data = array('error' => 'No hash!');
--- js/script.js
+++ js/script.js
@@ -2,18 +2,36 @@
  * Store the url for the request to the hash.php
  * @type {String}
  */
-var site = 'http://imiodrag.dyndns.org:8080/hash/hash.php';
+var site = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + "/hash.php";
+
+//Get
+
+var params = {};
+
+if (location.search) {
+    var parts = location.search.substring(1).split('&');
+
+    for (var i = 0; i < parts.length; i++) {
+        var nv = parts[i].split('=');
+        if (!nv[0]) continue;
+        params[nv[0]] = nv[1] || true;
+    }
+}
+
+var hash = params.hash;
 /**
  * Makes the ajax request to the hash.php and checks if there is data in the input field
- * 
+ *
  */
 function doJob()
 {
   $field = $('input.string-convert').val();
+  $data = ($field == '' && typeof hash != 'undefined') ? hash : $field;
   $err   = $('.error-holder');
-  if( $field != '')
+
+  if( $data != '')
   {
-    $.get(site,{hash: $field},function(data){
+    $.get(site,{hash: $data},function(data){
       if (typeof(data.error == "undefined")) {
         $.each(data,function(key,value){
           $('.'+key).html(value);
@@ -28,9 +46,15 @@ function doJob()
   }
 }
 $(function(){
-  $('form').on('submit',function(){
+
+    //Trigger via url
+    (typeof hash != 'undefined') ? doJob() : false;
+
+    //Form
+    $('form').on('submit',function(){
     doJob();
     return false;
-  });
-  $('input.string-convert').on('keyup',function(){ doJob(); });
+    });
+
+    $('input.string-convert').on('keyup',function(){ doJob(); });
 });
